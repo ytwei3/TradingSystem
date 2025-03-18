7daefdcb478ccf61ec11b00ce9fabc3d46234004
weiyaotian <yaotianwei03@gmail.com>
2025-03-18 11:28:23 +0800

    impl CRTP logger

--- include/trading/logger.hpp
+++ include/trading/logger.hpp
@@ -1,10 +1,46 @@
 #ifndef LOGGER_HPP
 #define LOGGER_HPP
 
+#include <chrono>
 #include <mutex>
+#include <print>
 #include <string>
 
 enum class LogLevel { INFO, WARNING, ERROR };
+
+inline auto getLogLevelString(LogLevel level) -> std::string {
+  switch (level) {
+  case LogLevel::INFO:
+    return "INFO";
+  case LogLevel::WARNING:
+    return "WARNING";
+  case LogLevel::ERROR:
+    return "ERROR";
+  default:
+    return "UNKNOWN";
+  }
+}
+
+// CRTP
+template <typename Derived> class LoggerBase {
+public:
+  void log(LogLevel level, const std::string &message) {
+    static_cast<Derived *>(this)->logImpl(level, message);
+  }
+};
+
+class ConsoleLogger : public LoggerBase<ConsoleLogger> {
+public:
+  void logImpl(LogLevel level, const std::string &message) {
+    std::print("[{}] [{}] {}\n", std::chrono::system_clock::now(),
+               getLogLevelString(level), message);
+  }
+};
+
+class FileLogger : public LoggerBase<FileLogger> {
+public:
+  void logImpl(LogLevel level, const std::string &message);
+};
 
 class Logger {
 public:
